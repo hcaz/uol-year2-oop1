5c74919a0075ae81ce936cfaebc57006a5cb7b49
Zachary Claret-Scott <github@hcaz.me.uk>
2015-11-27 19:46:05 +0000

    Updated matrix.h

--- year2-oop/year2-oop/Matrix.h
+++ year2-oop/year2-oop/Matrix.h
@@ -1,1 +1,15 @@
 #pragma once
+
+class Matrix
+{
+public:
+	Matrix();
+	~Matrix();
+	Matrix(int sizeR, int sizeC, double* input_data);
+	const double get(int i, int j);
+	const double set(int i, int j, double val);
+
+private:
+	int _M, _N;
+	double* _data;
+};
